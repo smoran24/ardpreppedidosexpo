23c99e3530c09377167db0093393dd8e10b1de91
Joaquin Bustos <P002490>
2023-12-12 14:35:07 +0000

    12/12 actualizacion destinations y dataSources

--- webapp/model/models.js
+++ webapp/model/models.js
@@ -16,7 +16,7 @@ sap.ui.define([
 		createHanaModel: function(){
            // var appid = this.getOwnerComponent().getManifestEntry("/sap.app/id").replaceAll(".","/");
           	var appModulePath = jQuery.sap.getModulePath("AR_DP_REP_PEDIDO_EXPO/AR_DP_REP_PEDIDO_EXPO");
-			let oModel = new ODataV2({"serviceUrl":appModulePath +  "/destinations/AR_DP_REP_DEST_HANA/ODATA_masterPedido.xsodata/"});
+			let oModel = new ODataV2({"serviceUrl":appModulePath +  "/destinations/AR_DP_DEST_HANA/ODATA_TablasMaestras.xsodata/"});
 			oModel.setUseBatch(false);
 			return oModel;
 		},
